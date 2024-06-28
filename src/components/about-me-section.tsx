fb66ccbcd3cdda2a876cc661786fd77fc87c9bd0
claudinxch <claudiohenrique0909@gmail.com>
2024-06-28 01:20:28 -0300

    first look at about me section

--- src/components/about-me-section.tsx
+++ src/components/about-me-section.tsx
@@ -4,17 +4,22 @@ import { useSection } from '../contexts/section'
 import { PrimaryButton } from './primary-button'
 import { ProfilePic } from './profile-pic'
 import { useTheme } from '../contexts/theme'
-import { Github, Linkedin } from 'lucide-react'
+import { Briefcase, Github, Linkedin } from 'lucide-react'
+import { useContactModal } from '../contexts/contact-modal'
+import { SecondaryButton } from './secondary-button'
 
 export const AboutMeSection = () => {
   const { changeId } = useSection()
   const { mode } = useTheme()
+  const { handleModalState } = useContactModal()
   return (
     <Container>
       <Title className="w-[180px]">About me</Title>
       <div className="flex h-[95%] justify-between">
-        <div className="max-h-[800px] overflow-x-hidden overflow-y-auto mr-6 flex lg:mr-0">
-          <div className="w-[90%] h-fit flex flex-col items-center p-8 mt-10 border-2 border-slate-200/10 rounded-xl justify-between lg:flex-row">
+        <div className="max-h-[800px] overflow-x-hidden overflow-y-auto mr-6 flex flex-col lg:mr-0">
+          <div
+            className={`w-[90%] h-fit flex flex-col items-center p-8 mt-10 border-2 rounded-xl justify-between ${mode === 'dark' ? 'border-slate-200/5' : 'border-[#D4D4D4]'} lg:flex-row`}
+          >
             <div className="order-0 lg:order-last">
               <ProfilePic
                 photoUrl="/img/myphoto2.jpg"
@@ -41,7 +46,7 @@ export const AboutMeSection = () => {
             </div>
             <div className="w-[82%] flex flex-col justify-between text-center mt-4 lg:h-[200px] lg:mt-0">
               <p
-                className={`text-[#EFF1F5] text-base tracking-tighter leading-6 break-words lg:text-left lg:tracking-normal lg:leading-normal`}
+                className={`text-base tracking-tighter leading-6 break-words ${mode === 'dark' ? 'text-[#EFF1F5]' : 'text-[#101010]'} lg:text-left lg:tracking-normal lg:leading-normal`}
               >
                 Hello! {"I'm"} Claudio. {"I'm"} a Front-end Developer with
                 excitement for building intuitive, efficient and visually
@@ -56,7 +61,9 @@ export const AboutMeSection = () => {
                 some cool songs.
               </p>
               <div className="mt-6">
-                <ul className="flex flex-col justify-between text-[#EFF1F5] gap-1 lg:flex-row">
+                <ul
+                  className={`flex flex-col justify-between gap-1 ${mode === 'dark' ? 'text-[#EFF1F5]' : 'text-[#101010]'} lg:flex-row`}
+                >
                   <li className="text-sm lg:text-base">
                     <strong className="custom-text">Age: </strong> 19
                   </li>
@@ -81,13 +88,23 @@ export const AboutMeSection = () => {
               </div>
             </div>
           </div>
+          <div className="w-full">
+            <div className="w-[530px] h-[190px] flex flex-col items-center justify-center gap-5 border-2">
+              <Briefcase />
+              <p>Full-time available</p>
+              <p>i am available for full-time positions</p>
+            </div>
+          </div>
+          <div className="flex justify-center mt-10">
+            <SecondaryButton
+              onClick={() => handleModalState(true)}
+              btnTitle={'Hire Me'}
+              className="px-6 py-5 lg:px-24 lg:py-7"
+            />
+          </div>
         </div>
         <div className="flex items-center">
-          <PrimaryButton
-            onClick={() => changeId()}
-            btnTitle={''}
-            className="p-5"
-          />
+          <PrimaryButton onClick={() => changeId()} className="p-5" />
         </div>
       </div>
     </Container>
