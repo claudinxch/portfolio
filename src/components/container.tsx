c6c015de7a15b40274a7c09db7e3c5779bd36352
claudinxch <claudiohenrique0909@gmail.com>
2024-07-01 12:22:39 -0300

    changed to section instead of div

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -11,7 +11,7 @@ export const Container = ({ className, ...props }: ContainerProps) => {
   const { isOpen } = useContactModal()
 
   return (
-    <div
+    <section
       className={`w-[calc(100% - 300px)] flex flex-col overflow-clip h-screen p-8 font-inter transition-colors duration-500 pb-4 xl:px-12 xl:pl-20  ${
         className || ''
       } ${mode === 'dark' ? 'container-dark-background' : 'container-light-background'} lg:ml-[300px]`}
@@ -19,7 +19,7 @@ export const Container = ({ className, ...props }: ContainerProps) => {
       {isOpen && <ContactModal />}
       <div className="flex-grow appearing-from-right">{props.children}</div>
       <Footer className={`mt-auto`} />
-    </div>
+    </section>
   )
 }
 // 2c2c2c
