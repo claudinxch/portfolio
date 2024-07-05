1294bb12d4987bd20813161cf908c2c5419abe4b
claudinxch <claudiohenrique0909@gmail.com>
2024-07-05 02:13:48 -0300

    added body color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react'
+import { Footer } from './components/footer'
 import { Navbar } from './components/navbar'
 import { Sections } from './components/sections'
 import { Switch } from './components/switch'
 import { useSection } from './contexts/section'
+import { useTheme } from './contexts/theme'
 
 function App() {
   const { sectionId } = useSection()
+  const { mode } = useTheme()
+  useEffect(() => {
+    document.body.style.backgroundColor =
+      mode === 'dark' ? '#101010' : '#EFF1F1'
+  }, [mode])
 
   return (
     <>
